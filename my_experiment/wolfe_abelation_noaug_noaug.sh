2c16484cc01930e7c74aa05c7375bc7d4a1bc759
sorawww31 <ee217092@meiji.ac.jp>
2024-10-03 18:23:06 +0900

    新しい実験

--- my_experiment/wolfe_abelation_noaug_noaug.sh
+++ my_experiment/wolfe_abelation_noaug_noaug.sh
@@ -1,14 +1,4 @@
 ## Ablation study
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2000000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2100000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2110000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment  --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111100000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111110000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment  --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment  --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111100 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment  --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111110 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111111 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
 
 python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2000000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_aug_no_aug --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
 python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2100000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_aug_no_aug --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
@@ -21,16 +11,16 @@ python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111100 --budget 
 python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111110 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_aug_no_aug --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
 python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111111 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_aug_no_aug --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
 
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2000000000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2100000000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2110000000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111000000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111100000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111110000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111100 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111110 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
-python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111111 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2000000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2100000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2110000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment  --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111100000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111110000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment  --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment  --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111100 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment  --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111110 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111111 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_no_aug_no_aug --no_paugment --noaugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
 
 python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2000000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_aug_aug --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
 python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2100000000 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_aug_aug --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
@@ -42,3 +32,14 @@ python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111000 --budget 
 python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111100 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_aug_aug --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
 python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111110 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_aug_aug --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
 python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111111 --budget 0.01 --restarts 8 --ensemble 1 --name ablation_aug_aug --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2000000000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2100000000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2110000000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111000000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111100000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111110000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111000 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111100 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111110 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111111 --budget 0.01 --restarts 8 --ensemble 8 --name ablation_no_aug_ens8 --no_paugment --table_path tables/wolfe_5_1_table_1/ --wolfe 0.9 1e-4
