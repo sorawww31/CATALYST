feee1c8a280ccc7c62e80f896f2a1f29430174fe
sorawww31 <ee217092@meiji.ac.jp>
2025-01-21 03:58:15 +0000

    ramdom

---
+++ my_experiment/random-subset.sh
@@ -0,0 +1,11 @@
+mkdir -p log/random-subset/
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2000000000  --budget 0.05 --restarts 8 --ensemble 1 --name random-subset --table_path tables/random-subset/ >& log/random-subset/2000000000.log
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2100000000  --budget 0.05 --restarts 8 --ensemble 1 --name random-subset --table_path tables/random-subset/ >& log/random-subset/2100000000.log
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2110000000  --budget 0.05 --restarts 8 --ensemble 1 --name random-subset --table_path tables/random-subset/ >& log/random-subset/2110000000.log
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111000000  --budget 0.05 --restarts 8 --ensemble 1 --name random-subset --table_path tables/random-subset/ >& log/random-subset/2111000000.log
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111100000  --budget 0.05 --restarts 8 --ensemble 1 --name random-subset --table_path tables/random-subset/ >& log/random-subset/2111100000.log
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111110000  --budget 0.05 --restarts 8 --ensemble 1 --name random-subset --table_path tables/random-subset/ >& log/random-subset/2111110000.log
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111000  --budget 0.05 --restarts 8 --ensemble 1 --name random-subset --table_path tables/random-subset/ >& log/random-subset/2111111000.log
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111100  --budget 0.05 --restarts 8 --ensemble 1 --name random-subset --table_path tables/random-subset/ >& log/random-subset/2111111100.log
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111110  --budget 0.05 --restarts 8 --ensemble 1 --name random-subset --table_path tables/random-subset/ >& log/random-subset/2111111110.log
+python brew_poison.py  --net ResNet18 --vruns 8 --poisonkey 2111111111  --budget 0.05 --restarts 8 --ensemble 1 --name random-subset --table_path tables/random-subset/ >& log/random-subset/2111111111.log
